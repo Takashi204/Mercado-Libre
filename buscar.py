d5c36053a239993ee4cc8954694a8daf6265b91a
Cristopher Montenegro <127951715+Takashi204@users.noreply.github.com>
2023-11-04 20:25:27 -0300

    ACTUALIZE dos pruebas

--- buscar.py
+++ buscar.py
@@ -2,34 +2,56 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+import unittest
+from datetime import datetime
 
-# Ruta al controlador de Chrome. Reemplázala con la ubicación de tu controlador.
-CHROME_DRIVER_PATH = 'C:\webdriver\chromedriver-win64\chromedriver.exe'
+class PruebaMercadoLibre(unittest.TestCase):
+    def setUp(self):
+        # Inicializar el navegador
+        self.driver = webdriver.Chrome()
+        self.driver.get('https://www.mercadolibre.cl/')
 
-# URL de Mercado Libre
-url = 'https://www.mercadolibre.cl/'
+    def test_buscar_producto(self):
+        try:
+            # Encontrar el campo de búsqueda y escribir el término de búsqueda
+            search_box = self.driver.find_element(By.NAME, 'as_word')
+            search_box.send_keys('iphone 13')
+            search_box.send_keys(Keys.RETURN)
 
+            # Esperar a que se carguen los resultados (20 segundos de espera)
+            time.sleep(15)
 
-# Término de búsqueda
-search_query = 'iphone 13'
+            # Realizar otras acciones si es necesario
 
-# Configuración del navegador
-chrome_options = webdriver.ChromeOptions()
+            self.assertTrue(True, "Búsqueda de producto exitosa.")
+        except Exception as e:
+            self.assertTrue(False, f"Error: {e}")
 
-# Inicializar el navegador
-driver = webdriver.Chrome(options=chrome_options)
+    def tearDown(self):
+        # Cerrar el navegador
+        self.driver.quit()
 
-# Abrir Mercado Libre
-driver.get(url)
+if __name__ == "__main__":
+    # Ejecutar las pruebas y generar un informe de texto
+    test_suite = unittest.TestLoader().loadTestsFromTestCase(PruebaMercadoLibre)
+    test_result = unittest.TextTestRunner(verbosity=2).run(test_suite)
 
-# Encontrar el campo de búsqueda y escribir el término de búsqueda
-search_box = driver.find_element(By.NAME, 'as_word')
-search_box.send_keys(search_query)
-search_box.send_keys(Keys.RETURN)
+    # Generar un informe de texto con la fecha y hora actual
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    report_file = f"Informe_Prueba_{now}.txt"
 
-# Esperar a que se carguen los resultados (puedes ajustar este tiempo según sea necesario)
-#driver.implicitly_wait(10)
-time.sleep(20)
+    with open(report_file, "w") as f:
+        f.write(f"Prueba realizada el {now}\n\n")
+        f.write(f"Resultado: {'Éxito' if test_result.wasSuccessful() else 'Fallo'}\n")
+        f.write(f"Total de pruebas: {test_result.testsRun}\n")
+        f.write(f"Pruebas exitosas: {test_result.testsRun - len(test_result.failures) - len(test_result.errors)}\n")
+        f.write(f"Pruebas fallidas: {len(test_result.failures)}\n")
+        f.write(f"Errores: {len(test_result.errors)}\n")
 
-# Cerrar el navegador
-driver.close()
+        if len(test_result.failures) > 0:
+            f.write("\nDetalles de las fallas:\n")
+            for failure in test_result.failures:
+                f.write(f"{failure[0]}\n")
+                f.write(f"{failure[1]}\n\n")
+        else:
+            f.write("\nLa prueba de busqueda de producto fue exitosa.\n")
--- prueba de caracteres.py
+++ prueba de caracteres.py
@@ -2,46 +2,65 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+import unittest
+from datetime import datetime
 
-# Ruta al controlador de Chrome. Reemplázala con la ubicación de tu controlador.
-CHROME_DRIVER_PATH = 'C:\webdriver\chromedriver-win64\chromedriver.exe'
+class PruebaMercadoLibre(unittest.TestCase):
+    def setUp(self):
+        # Inicializar el navegador
+        self.driver = webdriver.Chrome()
+        self.driver.get('https://www.mercadolibre.cl/')
 
-# URL de Mercado Libre
-url = 'https://www.mercadolibre.cl/'
+    def test_busqueda_con_caracteres_largos(self):
+        try:
+            # Término de búsqueda con más de 120 caracteres
+            search_query = 'a' * 121  # Esto creará una cadena de 121 caracteres "a"
 
+            # Encontrar el campo de búsqueda por su nombre
+            search_box = self.driver.find_element(By.NAME, 'as_word')
 
+            # Ingresar el término de búsqueda en el campo de búsqueda
+            search_box.send_keys(search_query)
 
+            # Realizar la búsqueda presionando la tecla Enter
+            search_box.send_keys(Keys.RETURN)
 
-# Configuración del navegador
-chrome_options = webdriver.ChromeOptions()
+            # Esperar a que se cargue la página de resultados (espera explícita de 10 segundos)
+            time.sleep(10)
 
-# Inicializar el navegador
-driver = webdriver.Chrome(options=chrome_options)
+            # Verificar si se muestra un mensaje de error por exceder el límite de caracteres
+            error_message = self.driver.find_element(By.CLASS_NAME, 'ui-search-no-result__text').text
+            expected_error_message = "Por favor, ingresa menos de 120 caracteres"
+            self.assertEqual(error_message, expected_error_message, "Mensaje de error incorrecto.")
 
-# Abrir Mercado Libre
-driver.get(url)
+        except Exception as e:
+            self.assertTrue(False, f"Error inesperado: {e}")
 
-# Término de búsqueda con un máximo de 120 caracteres
-search_query = 'a' * 120  # Esto creará una cadena de 120 caracteres "a"
+    def tearDown(self):
+        # Cerrar el navegador
+        self.driver.quit()
 
-# Encontrar el campo de búsqueda por su nombre
-search_box = driver.find_element(By.NAME, 'as_word')
+if __name__ == "__main__":
+    # Ejecutar las pruebas y generar un informe de texto
+    test_suite = unittest.TestLoader().loadTestsFromTestCase(PruebaMercadoLibre)
+    test_result = unittest.TextTestRunner(verbosity=2).run(test_suite)
 
-# Limitar el término de búsqueda a 120 caracteres
-search_query = search_query[:120]
+    # Generar un informe de texto con la fecha y hora actual
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    report_file = f"Informe_Prueba_{now}.txt"
 
-# Ingresar el término de búsqueda en el campo de búsqueda
-search_box.send_keys(search_query)
+    with open(report_file, "w") as f:
+        f.write(f"Prueba realizada el {now}\n\n")
+        f.write(f"Resultado: {'Éxito' if test_result.wasSuccessful() else 'Fallo'}\n")
+        f.write(f"Total de pruebas: {test_result.testsRun}\n")
+        f.write(f"Pruebas exitosas: {test_result.testsRun - len(test_result.failures) - len(test_result.errors)}\n")
+        f.write(f"Pruebas fallidas: {len(test_result.failures)}\n")
+        f.write(f"Errores: {len(test_result.errors)}\n")
 
-# Realizar la búsqueda presionando la tecla Enter
-search_box.send_keys(Keys.RETURN)
-
-# Esperar a que se carguen los resultados (puedes ajustar este tiempo según sea necesario)
-#driver.implicitly_wait(10)
-time.sleep(20)
-
-
-
-
-# Cerrar el navegador
-driver.close()
+        if len(test_result.failures) > 0:
+            f.write("\nDetalles de las fallas:\n")
+            for failure in test_result.failures:
+                f.write(f"{failure[0]}\n")
+                f.write(f"{failure[1]}\n\n")
+        else:
+            f.write("\nLa prueba fue fallida.\n")
